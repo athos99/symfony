47cdcf0b333c3bfab524ede74f0d95c89dcd9abc
athos99 <athos99@bluewin.ch>
2015-10-11 18:40:00 +0200

    dev

---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class LuckyController extends Controller
+{
+    /**
+     * @Route("/lucky/number/{count}", defaults={"count" = 1})
+     * @Template()
+     */
+    public function numberAction($count)
+    {
+        $numbers = array();
+        for ($i = 0; $i < $count; $i++) {
+            $numbers[] = rand(0, 100);
+        }
+        $numbersList = implode(', ', $numbers);
+
+        return array(
+            'number' => $numbersList,
+        );
+    }
+
+}
