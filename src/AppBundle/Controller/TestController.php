1b97b8ddd63b25c52e6831b6a454355afa4ad719
unknown <BOBILLIERM@FS-282884.ge-admin.ad.etat-ge.ch>
2015-11-10 17:07:19 +0100

    dev

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -4,14 +4,16 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class TestController extends Controller
-{
-    public function indexAction()
-    {
+class TestController extends Controller {
+  public function indexAction() {
 
-        $tests = array('url' => $this->generateUrl(          'blog_show',          array('slug' => 'my-blog-post')        ));
-        return $this->render('AppBundle:Test:index.html.twig', array(
-                // ...
-            ));    }
+    $tests = array(
+      array('url' => $this->generateUrl('test1'), 'title' => 'test1'),
+      array('url' => $this->generateUrl('test2'), 'title' => 'test2'),
+
+    );
+    return $this->render('AppBundle:Test:index.html.twig',
+      array('links' => $tests));
+  }
 
 }
