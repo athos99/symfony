bf9a626bf14b15f586969189d350f4a56acf4881
unknown <BOBILLIERM@FS-282884.ge-admin.ad.etat-ge.ch>
2015-11-13 17:34:23 +0100

    dev

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -3,6 +3,9 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Product;
+use AppBundle\Entity\Category;
+
 
 class TestController extends Controller {
   public function indexAction() {
@@ -15,7 +18,26 @@ class TestController extends Controller {
     return $this->render('AppBundle:Test:index.html.twig',
       array('links' => $links));
   }
+
   public function index1Action() {
+    $em = $this->getDoctrine()->getManager();
+    $product = new Product();
+    $product->setName('xxxx');
+    $em->persist($product);
+    $em->flush();
+    $idProduct = $product->getId();
+
+
+    $repository = $this->getDoctrine()->getRepository('AppBundle:Product');
+
+    $product = $repository->find($idProduct);
+
+    $product = $repository->findOneById($idProduct);
+    $product = $repository->findOneByName('xxxx');
+
+    $product = $em->getRepository('AppBundle:Product')->find($idProduct);
+    $product->setName('yyyy');
+    $em->flush();
 
     $links = array(
       array('href' => $this->generateUrl('test1'), 'text' => '<b>test1</b>'),
@@ -25,6 +47,7 @@ class TestController extends Controller {
     return $this->render('AppBundle:Test:index.html.twig',
       array('links' => $links));
   }
+
   public function index2Action() {
 
     $links = array(
